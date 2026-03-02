1ae2d486266c50d6fabd69869a2db64d69204b98
pelicanodionejulia <pelicano.dionejulia.bsis@gmail.com>
2026-03-02 22:04:06 +0800

    Add files via upload

---
+++ app.py
@@ -0,0 +1,10 @@
+print("=============================")
+print("   MULTIPLICATION CALCULATOR  ")
+print("=============================")
+
+PelicanoNumber1 = int(input("Enter first number: "))
+PelicanoNumber2 = int(input("Enter second number: "))
+
+PelicanoProduct = PelicanoNumber1 * PelicanoNumber2
+
+print(f"Answer: {PelicanoProduct}")
